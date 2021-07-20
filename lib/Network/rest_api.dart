8b5fc739e7898d6673bbff9d77d94e81bf409ef1
sudo <perasutlife@gmail.com>
2021-07-20 10:07:57 +0700

    Day4

---
+++ lib/Network/rest_api.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'package:smart_news/Model/LoginModel.dart';
+import 'package:smart_news/Utils/constant.dart';
+
+class CallAPI {
+  // กำหนด header ของ api
+  _setHeaders() =>
+      {'Content-Type': 'application/json', 'Accept': 'application/json'};
+
+  // กำหนด header สำหรับ Auth
+  _setHeadersAuth() => {'Content-Type': 'application/x-www-form-urlencoded'};
+
+  // Login Auth
+  Future<bool> loginCustomer(String username, String password) async {
+    var response = await http.post(Uri.parse(baseAPIURL + ' '),
+        headers: _setHeadersAuth(),
+        body: {"username": username, "password": password});
+
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      LoginModel loginModel = loginModelFromJson(jsonString);
+
+      return loginModel.statusCode == 200 ? true : false;
+    }
+
+    return false;
+  }
+}
--- lib/Screens/Login/LoginScreen.dart
+++ lib/Screens/Login/LoginScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:smart_news/Themes/colors.dart';
+import 'package:smart_news/Network/rest_api.dart';
+import 'package:smart_news/Utils/form_helper.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -7,18 +8,185 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // กำหนดตัวแปรเก็บ key
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  static final GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+
+  // กำหนดตัวแปรรับค่า username / password
+  String _username = "";
+  String _password = "";
+  bool hidePassword = true;
+  bool isAPICallProcess = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: Text('Login', style: TextStyle(color: textColorSecondary),),
-      ),
-      body: Center(
-        child: Text('Login content'),
+    return SafeArea(
+      child: Scaffold(
+        key: _scaffoldKey,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+        ),
+        body: isAPICallProcess
+            ? Center(child: CircularProgressIndicator())
+            : GestureDetector(
+                onTap: () {
+                  FocusScope.of(context).unfocus();
+                },
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: globalFormKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 200,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    Color(0xffe64980),
+                                    Color(0xffff8787)
+                                  ]),
+                              borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(80))),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Align(
+                                alignment: Alignment.center,
+                                child: CircleAvatar(
+                                  radius: 50.0,
+                                  backgroundImage:
+                                      AssetImage('images/SignInLogo.png'),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Center(
+                          child: Padding(
+                            padding: EdgeInsets.only(bottom: 20, top: 40),
+                            child: Text(
+                              "Login",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 25),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            top: 20.0,
+                            bottom: 20.0,
+                          ),
+                          child: FormHelper.inputFieldWidget(
+                              context,
+                              Icon(Icons.verified_user),
+                              "username",
+                              "Username", (onValidateVal) {
+                            if (onValidateVal.isEmpty) {
+                              return 'Username is required';
+                            }
+                            return null;
+                          }, (onSavedVal) {
+                            _username = onSavedVal;
+                          }),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(
+                            bottom: 20.0,
+                          ),
+                          child: FormHelper.inputFieldWidget(
+                              context,
+                              Icon(Icons.verified_user),
+                              "password",
+                              "Password", (onValidateVal) {
+                            if (onValidateVal.isEmpty) {
+                              return 'Password is required';
+                            }
+                            // else if(!validateStructure(onValidateVal)){
+                            //   return 'Password not math with pattern';
+                            // }
+                            return null;
+                          }, (onSavedVal) {
+                            _password = onSavedVal;
+                          },
+                              obscureText: hidePassword,
+                              suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    hidePassword = !hidePassword;
+                                  });
+                                },
+                                color: Colors.redAccent.withOpacity(0.9),
+                                icon: Icon(hidePassword
+                                    ? Icons.visibility_off
+                                    : Icons.visibility),
+                              )),
+                        ),
+                        SizedBox(
+                          height: 20.0,
+                        ),
+                        Center(
+                          child: FormHelper.saveButton("Login", () {
+                            if (validateAndSave()) {
+                              print("Username: $_username");
+                              print("Password: $_password");
+
+                              setState(() {
+                                // โชว์ progressbar
+                                this.isAPICallProcess = true;
+                              });
+
+                              // เรียกใช้ API เพื่อล็อกอิน
+                              CallAPI()
+                                  .loginCustomer(_username, _password)
+                                  .then((response) {
+                                setState(() {
+                                  // ซ่อน Progressbar
+                                  this.isAPICallProcess = false;
+                                });
+
+                                print(response);
+
+                                if (response) {
+                                  // ส่งไปหน้า dashboard
+                                  Navigator.pushReplacementNamed(
+                                      context, '/dashboard');
+                                }
+                              });
+                            }
+                          }),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
       ),
     );
+  }
+
+  bool validateStructure(String value) {
+    String pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    RegExp regExp = new RegExp(pattern);
+    return regExp.hasMatch(value);
+  }
+
+  /* Validate and Save form */
+  bool validateAndSave() {
+    final form = globalFormKey.currentState;
+    if (form!.validate()) {
+      form.save();
+      return true;
+    }
+
+    return false;
   }
 }
